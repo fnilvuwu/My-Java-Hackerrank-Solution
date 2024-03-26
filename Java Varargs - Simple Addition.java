4d2b8e284106b4128f94fe8bba5f11de2d3dcaff
fnilvuwu <mamatmks45@gmail.com>
2024-03-26 15:42:48 +0800

    new file:   Java Varargs - Simple Addition.java

---
+++ Java Varargs - Simple Addition.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add {
+    // A method that takes variable 
+    // number of integer arguments.
+    static void add(int... a)
+    {
+        // using for each loop to display contents of a
+        int sum = 0;
+        int currentIndex = 0;
+        for (int i : a) {
+            sum += i;
+            currentIndex++;
+            if (currentIndex < a.length) {
+                System.out.print(i + "+");
+            } else {
+                System.out.print(i);
+            }
+        }
+        System.out.println("=" + sum);
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
+}
+
