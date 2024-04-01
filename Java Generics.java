80e0c5101ec71c4243f040fa4a796ce0e1c889b2
fnilvuwu <mamatmks45@gmail.com>
2024-04-02 01:26:55 +0800

    modified:   Java Generics.java

--- Java Generics.java
+++ Java Generics.java
@@ -1,9 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+class Solution<T> {
+    T obj;
+    
+    Solution(T obj) {
+        this.obj = obj;
+    }
+    
+    public void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+}
+
+public class Main {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        
+        Solution<Integer> intSolution = new Solution<>(1);
+        Solution<String> stringSolution = new Solution<>("Hello");
+        
+        intSolution.printArray(intArray);
+        
+        stringSolution.printArray(stringArray);
     }
 }
